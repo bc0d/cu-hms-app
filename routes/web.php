a665e16b95c055d846872d3fab6dac6f23ca39e0
mumtazuk <mumthazsulthana123@gmail.com>
2024-04-29 16:39:25 +0530

    test1

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,29 @@ use App\Http\Controllers\Office\RuleAndNoticeController;
 */
 
 
+/**Route::get('/', function () {
+    return view('users.index');
+});
+
+Route::get('home', function () {
+    return view('users.homePage');
+});
+
+Route::get('user-login', function () {
+    return view('users.login');
+});
+
+Route::get('user-signup', function () {
+    return view('users.signup_basic');
+});
+
+Route::get('user-signup-dtls', function () {
+    return view('users.signup_details');
+});
+
+Route::get('user-mail-confirm', function() {
+    return view('users.mailconfirm');
+});
 Route::get('/', function () {
     return view('users.index');
 });
@@ -47,3 +70,33 @@ Route::get('user-signup-dtls', function () {
 Route::get('user-mail-confirm', function() {
     return view('users.mailconfirm');
 });
+Route::get('user-home', function() {
+    return view('users.homePage');
+});
+Route::get('user-profile', function() {
+    return view('users.profile');
+});
+Route::get('user-profile-detials',function(){
+    return view('users.profiledetialed');
+});
+Route::get('testing',function(){
+    return view('users.testing');
+});
+Route::get('feedback',function(){
+    return view('users.feedback');
+})->name('feedback');
+Route::get('payment-link',function(){
+    return view('users.paymentlink');
+})->name('paymentlink');
+Route::get('complaint',function(){
+    return view('users.complaint');
+})->name('complaint');
+Route::get('messbill',function(){
+    return view('users.messbill');
+})->name('messbillfp');
+Route::get('notificationfull',function(){
+    return view('users.notificationfull');
+});
+Route::get('attendance',function(){
+    return view('users.attendance');
+}); */
