5961ecd2bf20d65db1ea7deb497267111f7a8316
bc0d <athulkrishna446@gmail.com>
2024-05-21 15:28:52 +0530

    public side editing

--- resources/views/users/room_rent_payment.blade.php
+++ resources/views/users/room_rent_payment.blade.php
@@ -4,37 +4,42 @@
 <!-- ======= Contact Section ======= -->
 <section id="services" class="services">
   <h2 class="sign-sec  text-center">Room Rent Payment</h2><hr>
-<div class="container col-lg-8 hi mt-5  align-items-center py-5">
-<h1 class="heading">ROOM RENT BILL PAYMENT</h1>
-<table class="table">
-    <thead>
-      <tr>
-        <th>Month</th>
-        <th>Bill Amount</th>
-        <th>#</th>
-        
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td data-label="Month">april</td>
-        <td data-label="Bill Amount">70</td>
-        <td data-label="#"><a href="#" class="btn">Pay Now</a></td> 
-      </tr>
-      <tr>
-        <td data-label="Month">may</td>
-        <td data-label="Bill Amount">70</td>
-        <td data-label="#"><a href="#" class="btn">Pay Now</a></td> 
-      </tr>
-      <tr>
-        <td data-label="Month">june</td>
-        <td data-label="Bill Amount">70</td>
-        <td data-label="#"><a href="#" class="btn">Pay Now</a></td> 
-      </tr>
-      
-      <!-- Add more rows as needed -->
-    </tbody>
-</table>
-</div>
+  <div class="container col-lg-8 hi mt-5  align-items-center py-5">
+    <div class="row gy-4 gy-lg-0">
+      <div class="col-12">
+        <h1 class="heading">ROOM RENT BILL PAYMENT</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th>Month</th>
+                <th>Bill Amount</th>
+                <th>#</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Month">april</td>
+                <td data-label="Bill Amount">70</td>
+                <td data-label="#"><a href="#" class="btn">Pay Now</a></td> 
+              </tr>
+              <tr>
+                <td data-label="Month">may</td>
+                <td data-label="Bill Amount">70</td>
+                <td data-label="#"><a href="#" class="btn">Pay Now</a></td> 
+              </tr>
+              <tr>
+                <td data-label="Month">june</td>
+                <td data-label="Bill Amount">70</td>
+                <td data-label="#"><a href="#" class="btn">Pay Now</a></td> 
+              </tr>
+              
+              <!-- Add more rows as needed -->
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </section>
+
 @endsection
