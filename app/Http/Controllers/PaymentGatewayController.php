96914e494adb587d89605ee26894c90a0eff99b3
bc0d <athulkrishna446@gmail.com>
2024-06-04 05:13:20 +0530

    payment completed

--- app/Http/Controllers/PaymentGatewayController.php
+++ app/Http/Controllers/PaymentGatewayController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use App\Models\RoomRent;
+use App\Models\WaterElectricBill;
 
 class PaymentGatewayController extends Controller
 {
@@ -21,8 +23,8 @@ class PaymentGatewayController extends Controller
             'student_id' => 'required|string',
             'purpose' => 'required|string',
             'amount' => 'required|numeric',
-            'callback_url' => 'required',
             'status' => 'required|string',
+            'purpose_id' => 'required|string',
         ]);
 
         $transaction = new Transaction();
@@ -30,16 +32,25 @@ class PaymentGatewayController extends Controller
         $transaction->purpose = $data['purpose'];
         $transaction->amount = $data['amount'];
         $transaction->status = $data['status'];
+        $transaction->purpose_id = $data['purpose_id'];
         $transaction->save();
 
+        if($data['purpose'] === 'Room Rent') {
+
+            $rent = RoomRent::findOrFail($data['purpose_id']);
+            $rent->payment_date = $transaction->created_at;
+            $rent->transaction_id = $transaction->transaction_id;
+            $rent->paid_status = 'Success';
+            $rent->save();
+        } elseif($data['purpose'] === 'Electric and Water') {
+
+            $bill = WaterElectricBill::findOrFail($data['purpose_id']);
+            $bill->payment_date = $transaction->created_at;
+            $bill->transaction_id = $transaction->transaction_id;
+            $bill->paid_status = 'Success';
+            $bill->save();
+        }
+
         return redirect()->back();
-    }
-    
-    public function showPayMonthly() {
-
-        $paymentDetails = session('paymentDetails');
-
-        // Use $paymentDetails as needed
-         return view('payment.payment_gateway', compact('paymentDetails'));
     }
 }
--- app/Http/Controllers/User/UserFeeAndPaymentController.php
+++ app/Http/Controllers/User/UserFeeAndPaymentController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\RoomRent;
 use App\Models\WaterElectricBill;
-
+use App\Models\Transaction;
 use App\Models\Student;
 use App\Models\Fee;
 use Carbon\Carbon;
@@ -31,7 +31,28 @@ class UserFeeAndPaymentController extends Controller
 
         $student = Auth::guard('students')->user();
         $rents = RoomRent::findOrFail($id);
-        return view('users.room_rent_payment', compact('student', 'rents'));
+        return view('users.room_rent_payment', compact('student', 'rents'));        
+    }
+
+    public function callPaymentGatewayRent(Request $request) {
+
+        $student = Auth::guard('students')->user();
+        $data = $request->validate([
+            'id' => 'required|string',
+            'student' => 'required|string',
+            'amount' => 'required|string',
+        ]);
+
+        $paymentDetails = [
+            'student_id' => $data['student'],
+            'purpose' => 'Room Rent',
+            'amount' => $data['amount'],
+            'purpose_id' => $data['id'],
+            'callback_url' => route('room.callback'),
+        ];
+
+        session()->flash('paymentDetails', $paymentDetails);
+        return redirect('/payment-gateway');
     }
 
     public function viewBills() {
@@ -40,5 +61,33 @@ class UserFeeAndPaymentController extends Controller
         $bills = WaterElectricBill::where('student_id', $student->student_id)->get();
 
         return view('users.room_elecwat_status',compact('student','bills'));
+    }
+
+    public function payWaterElectricBill($id) {
+
+        $student = Auth::guard('students')->user();
+        $bills = WaterElectricBill::findOrFail($id);
+        return view('users.room_elecwat_payment', compact('student', 'bills'));   
+    }
+
+    public function callPaymentGatewayBills(Request $request) {
+
+        $student = Auth::guard('students')->user();
+        $data = $request->validate([
+            'id' => 'required|string',
+            'student' => 'required|string',
+            'amount' => 'required|string',
+        ]);
+
+        $paymentDetails = [
+            'student_id' => $data['student'],
+            'purpose' => 'Electric and Water',
+            'amount' => $data['amount'],
+            'purpose_id' => $data['id'],
+            'callback_url' => route('room.callback'),
+        ];
+
+        session()->flash('paymentDetails', $paymentDetails);
+        return redirect('/payment-gateway');
     }
 }
